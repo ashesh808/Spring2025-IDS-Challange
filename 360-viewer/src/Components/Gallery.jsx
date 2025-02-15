4991467941ffbe4b937484cb861b43f30426af8b
Andrew Clayton <126101983+andrew-clayton@users.noreply.github.com>
2025-02-15 12:21:01 -0600

    Removing backend generated files

--- 360-viewer/src/Components/Gallery.jsx
+++ 360-viewer/src/Components/Gallery.jsx
@@ -1,9 +1,13 @@
-
+import { useState } from 'react';
+import GalleryIcon from './GalleryIcon';
 
 export default function Gallery() {
-
+    const [panos, setPanos] = useState(["test1", "test2", "test3"]);
+    console.log("rendering gallery component");
 
     return (
-        <p>Gallery placeholder</p>
+        <>
+            {panos.map(pano => <GalleryIcon key={pano} pano={pano} />)}
+        </>
     );
 }
