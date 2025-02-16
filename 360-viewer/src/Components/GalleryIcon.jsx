5641d31e4671b21072a262c9060ad1690baa2da3
Serenity Vang <serenity@Serenitys-M4-MacBook-Pro.local>
2025-02-15 18:26:48 -0600

    added static photos for the gallery

--- 360-viewer/src/Components/GalleryIcon.jsx
+++ 360-viewer/src/Components/GalleryIcon.jsx
@@ -2,12 +2,16 @@
 
 export default function GalleryIcon({ name, id, ath, atv, type, description, pdf, video }) {
 
+    const imagePath = `/images/poi${id}.png`;  
+
     return (
         <>
             {/* Preview */}
             <div className="gallery-card">
                 <div className="card-content">
+                    <img src={imagePath} alt="{name}" className="card-image"/>
                     <div>
+                    <p>Image Path: {imagePath}</p>   {/* Let's keep this temporarily to debug */}
                         <p>Description: {description}</p>
                         <p>ath: {ath}</p>
                     </div>
