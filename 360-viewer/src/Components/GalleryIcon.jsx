3121f7f8086a3f905d95462c3896716cf695491e
ToothlessBrush <johnklein514@gmail.com>
2025-02-15 20:03:18 -0600

    add form to create poi

--- 360-viewer/src/Components/GalleryIcon.jsx
+++ 360-viewer/src/Components/GalleryIcon.jsx
@@ -1,9 +1,16 @@
-
 import { useNavigate } from "react-router-dom";
 
-export default function GalleryIcon({ name, id, ath, atv, type, description, pdf, video }) {
-
-    const imagePath = `/images/poi${id}.png`;  
+export default function GalleryIcon({
+    name,
+    id,
+    ath,
+    atv,
+    type,
+    description,
+    pdf,
+    video,
+}) {
+    const imagePath = `/images/poi${id}.png`;
 
     const navigate = useNavigate();
 
@@ -16,12 +23,15 @@ export default function GalleryIcon({ name, id, ath, atv, type, description, pdf
             {/* Preview */}
             <div className="gallery-card">
                 <div className="card-content">
-                    <img src={imagePaths} alt="{name}" className="card-image"/>
+                    <img src={imagePath} alt="{name}" className="card-image" />
                     <div>
-                        <p>Image Path: {imagePath}</p>   {/* Let's keep this temporarily to debug */}
+                        <p>Image Path: {imagePath}</p>{" "}
+                        {/* Let's keep this temporarily to debug */}
                         <p>Description: {description}</p>
                         <p>ath: {ath}</p>
-                        <button onClick={handleViewClick}>Go to View {id}</button>
+                        <button onClick={handleViewClick}>
+                            Go to View {id}
+                        </button>
                     </div>
                 </div>
             </div>
